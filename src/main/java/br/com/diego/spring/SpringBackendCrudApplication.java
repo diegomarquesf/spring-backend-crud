d691495d8f8c9eae8d33a9b2a251be73823c08c6
Diego <fariia_di@hotmail.com>
2022-06-08 11:28:24 -0300

    criacao do projeto

---
+++ src/main/java/br/com/diego/spring/SpringBackendCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.diego.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendCrudApplication.class, args);
+	}
+
+}
